792bdb3b1695a0e88fde1f36769432049ecac3d6
vestr3k <appleduckkael@gmail.com>
2026-08-21 17:13:47 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
 import {
     getFirestore
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import {
+    getAuth,
+    GoogleAuthProvider
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyB_JURWbgsOLzDxqD_zTzc1fIzVNUMxiSw",
@@ -13,6 +22,12 @@ const firebaseConfig = {
     measurementId: "G-7Y8VWRRPJ8"
 };
 
+
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
+export const auth = getAuth(app);
+
+export const googleProvider =
+    new GoogleAuthProvider();
